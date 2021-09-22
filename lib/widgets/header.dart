038bef48a332bb27473abe268c6df974dcea90ed
NGUYỄN VĂN QUÂN <anhquan.lien123@gmail.com>
2021-09-22 23:36:09 +0700

    add header facebook ui

---
+++ lib/widgets/header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HeaderNavigation extends StatefulWidget {
+  HeaderNavigation({Key? key}) : super(key: key);
+
+  @override
+  _HeaderNavigationState createState() => _HeaderNavigationState();
+}
+
+class _HeaderNavigationState extends State<HeaderNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
