a8ff3297cae362bc3f76a00314d0fcdd8441da60
NGUYỄN VĂN QUÂN <anhquan.lien123@gmail.com>
2021-09-24 01:46:31 +0700

    add home

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_website_1/constants/constants.dart';
 import 'package:flutter_website_1/constants/string_value.dart';
 import 'package:flutter_website_1/constants/widget_export.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -19,7 +20,7 @@ class _HomeScreenState extends State<HomeScreen>
   void initState() {
     super.initState();
     controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
     animation =
         IntTween(begin: 0, end: titleHomeInfo.length).animate(controller);
     controller.repeat(reverse: true);
@@ -34,15 +35,40 @@ class _HomeScreenState extends State<HomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backgroudPageColor,
-      body: Column(
-        children: [
-          AnimatedBuilder(
-              animation: animation,
-              builder: (context, child) =>
-                  Text(titleHomeInfo.substring(0, animation.value) + "_"))
-        ],
-      ),
-    );
+        backgroundColor: backgroudPageColor,
+        body: Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/background4.jpg"),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {
+                      Alert(
+                              context: context,
+                              title: "Thông Báo ",
+                              desc: "Bạn Vừa ckick vào trang chủ")
+                          .show();
+                    },
+                    child: Container(
+                        height: 60,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            color: whiteColor,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: const Image(
+                            fit: BoxFit.cover,
+                            image: AssetImage("assets/images/logo.png"))),
+                  )
+                ],
+              ),
+            )));
   }
 }
